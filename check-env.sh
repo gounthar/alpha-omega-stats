eee8011eeb5fefda806ecc3175bf39918a389394
gounthar <gounthar@gmail.com>
2025-02-25 11:36:46 +0100

    fix(check-env): Correctly export JAVA_HOME after setting its value

--- check-env.sh
+++ check-env.sh
@@ -62,7 +62,8 @@ if [ -z "${JAVA_HOME-}" ]; then
   # Try to infer JAVA_HOME from java command path
   if command -v java >/dev/null; then
     temp_java=$(command -v java)
-    export JAVA_HOME=$(dirname "$(dirname "$(readlink -f "$temp_java")")")
+    JAVA_HOME=$(dirname "$(dirname "$(readlink -f "$temp_java")")")
+    export JAVA_HOME
     # Print a success message in green
     success "JAVA_HOME set to $JAVA_HOME"
   else
