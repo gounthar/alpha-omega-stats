3478c50842083cbc048c35d337cb930f92a32f6b
gounthar <gounthar@gmail.com>
2025-03-07 22:11:19 +0100

    feat: Add entrypoint script to validate GITHUB_TOKEN and run jenkins-pr-collector

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "Error: GITHUB_TOKEN environment variable is required"
+    exit 1
+fi
+
+END_DATE=$(date +%Y-%m-%d)
+./jenkins-pr-collector -github-token "$GITHUB_TOKEN" -start-date "$START_DATE" -end-date "$END_DATE" 
